516520591cd0daa5cdf07d471d3e0b75ad42124a
Shivam Chaurasia <Shivamchaurasiya612@gmail.com>
2023-10-05 22:03:36 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,93 @@
+// section5
+function bullet(){
+    document.getElementById("MotorImg").src="./images/Bullet 350.jpeg";
+}
+function interceptor(){
+    document.getElementById("MotorImg").src="./images/interceptor.jpg";
+}
+function Continenral(){
+    document.getElementById("MotorImg").src="./images/Continenral.jpg";
+}
+function interceptor(){
+    document.getElementById("MotorImg").src="./images/interceptor.jpg";
+}
+function superMeteror(){
+    document.getElementById("MotorImg").src="./images/super.avif";
+}
+function hunter(){
+    document.getElementById("MotorImg").src="./images/hunter.avif";
+}
+function scram(){
+    document.getElementById("MotorImg").src="./images/scram.avif";
+}
+function classic(){
+    document.getElementById("MotorImg").src="./images/classic.jpeg";
+}
+function meteor(){
+    document.getElementById("MotorImg").src="./images/meteor.jpg";
+}
+function himalayan(){
+    document.getElementById("MotorImg").src="./images/himalayan.jpg";
+}
+// section6
+function Motoverse(){
+    document.getElementById("RidesImg").src="./images/motoverse.webp";
+}
+function Uncover(){
+    document.getElementById("RidesImg").src="./images/uncover.jpeg";
+}
+function Track(){
+    document.getElementById("RidesImg").src="./images/track.avif";
+}
+function Himalayan2023(){
+    document.getElementById("RidesImg").src="./images/himalayanadventure.avif";
+}
+function GT(){
+    document.getElementById("RidesImg").src="./images/gt.jpeg";
+}
+function HimalayanOdyssey(){
+    document.getElementById("RidesImg").src="./images/himalayanodyssey.avif";
+}
+function Leh(){
+    document.getElementById("RidesImg").src="./images/leh.jpg";
+}
+function RE(){
+    document.getElementById("RidesImg").src="./images/RE.jpg";
+}
+// section7
+function aom(){
+    document.getElementById("ApparelImg").src="./images/artapparel.jpeg";
+}
+function sm650(){
+    document.getElementById("ApparelImg").src="./images/superapparel.jpg";
+}
+function classicapparel(){
+    document.getElementById("ApparelImg").src="./images/classicapparel.jpg";
+}
+function royalenfieldapparel(){
+    document.getElementById("ApparelImg").src="./images/royalenfieldapparel.jpg";
+}
+function royalenfieldtcxapparel(){
+    document.getElementById("ApparelImg").src="./images/royalenfieldtcxapparel.avif";
+}
+function miyridingjacketapparel(){
+    document.getElementById("ApparelImg").src="./images/miy-riding-jacketapparel.avif";
+}
+// section8
+function gmaaccessories(){
+    document.getElementById("AccessoriesImg").src="./images/gmaaccessories.avif";
+}
+function miyaccessories(){
+    document.getElementById("AccessoriesImg").src="./images/miyaccessories.jpg";
+}
+// section9
+function stores(){
+    document.getElementById("LocateImg").src="./images/storeslocate.jpg";
+}
+function sc(){
+    document.getElementById("LocateImg").src="./images/service-centreslocate.avif";
+}
+function genuine(){
+    document.getElementById("LocateImg").src="./images/genuine-partslocate.jpg";
+}
+
